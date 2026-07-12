081da2f421b918e7c93394dc7186854efc08c2dc
Mohanakannan <pappukannan777@gmail.com>
2026-07-12 21:33:15 +0530

    python test 2

---
+++ module 3/test 1/test1.py
@@ -0,0 +1,18 @@
+def add (a,b):
+    return (a+b)
+def sub (a,b):
+    return (a-b)
+def multi (a,b):
+    return (a*b)
+def divi (a,b):
+    return (a/b)
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+add_result=add(a,b)
+print("add result:",add_result)
+sub_result=sub(a,b)
+print("sub result:",sub_result)
+multi_result=multi(a,b)
+print("multi result:",multi_result)
+divi_result=divi(a,b)
+print("divi result:",divi_result)
