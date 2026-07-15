21a947a09bafa91a86e6270a4064df799762ca81
Mohanakannan <pappukannan777@gmail.com>
2026-07-15 21:42:50 +0530

    lesson 1

---
+++ module 4/lesson 1/activity1.py
@@ -0,0 +1,10 @@
+empty_list=[]
+print(empty_list)
+number=[1,2,3,4,5]
+print(number)
+list_1=[1,2,3]
+triples=list_1*3
+print (triples)
+alist=[100,200,300,400,500]
+alist=alist[::-1]
+print(alist)
---
+++ module 4/lesson 1/activity2.py
@@ -0,0 +1,7 @@
+l=[4,5,1,2,9,7,10,8]
+print(l)
+print("lenght of the list is ",len(l))
+l.sort()
+print("list",l)
+print("smallest element in list is",l[0])
+print("largest element in list is",l[-1])
