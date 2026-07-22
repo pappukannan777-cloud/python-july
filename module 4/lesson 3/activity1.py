d371e7ead28a2bffafd24fc389bd20ac410d8303
Mohanakannan <pappukannan777@gmail.com>
2026-07-22 21:47:53 +0530

    lesson 3

---
+++ module 4/lesson 3/activity1.py
@@ -0,0 +1,10 @@
+country_code={'India':'0091',
+              'australia' : '0025',
+              'japan':'0224',
+              'nepal':'00977'}
+print("country code for india -")
+print(country_code.get('India','Not found'))
+print("country code for japan -")
+print(country_code.get('japan','Not found'))
+print("country code for canada -")
+print(country_code.get('canada','Not found'))
